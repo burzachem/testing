787785d1d4abb6043eaff433921ccc56b40e0293
burzachem <67247458+burzachem@users.noreply.github.com>
2020-10-30 09:56:34 -0600

    ok
    new update

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: ["Are you looking to get a good website?", "Do you feel like giving up?", "Don't stop! keep going.", "You will get there.", "welcome to online classes 1."],
+        typeSpeed: 1
+      });
+  });
